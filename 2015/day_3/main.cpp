930d51cd31901c05d57f2da9e449a671b7088df0
alaidine <aa.alimlinde@gmail.com>
2023-01-16 21:07:20 +0400

    first part of day 3 solved

--- 2015/day_3/main.cpp
+++ 2015/day_3/main.cpp
@@ -2,13 +2,13 @@
 #include <string>
 #include <fstream>
 #include <vector>
+#include <set>
 
 using namespace std;
 
 vector<int> go_north(vector<int> current_position) {
   return {current_position[0], current_position[1]+1};
 }
-
 
 vector<int> go_south(vector<int> current_position) {
   return {current_position[0], current_position[1]-1};
@@ -19,7 +19,30 @@ vector<int> go_west(vector<int> current_position) {
 }
 
 vector<int> go_east(vector<int> current_position) {
-  return {current_position[0]+1, current_position[1]+1};
+  return {current_position[0]+1, current_position[1]};
+}
+
+vector<int> update_position(vector<int> current_position, char direction) {
+  vector<int> new_position;
+
+  switch (direction) {
+    case '^':
+      return go_north(current_position);
+      break;
+    case 'v':
+      return go_south(current_position);
+      break;
+    case '<':
+      return go_west(current_position);
+      break;
+    case '>':
+      return go_east(current_position);
+      break;
+    default:
+      return current_position;
+  }
+
+  return current_position;
 }
 
 int read_file(string input) {
@@ -28,19 +51,20 @@ int read_file(string input) {
   string file_read;
 
 
-  int number_of_houses_visited = 1;
-  vector<vector<int>> history_of_positions = {
+  vector<int> current_position = {0, 0};
+  set<vector<int>> history_of_positions = {
     {0, 0}
   };
+
   while (getline(file, line)) {
     for (char c : line) {
-      cout << c << endl;
+      current_position = update_position(current_position, c);
+      history_of_positions.insert(current_position);
     }
   }
 
-  return number_of_houses_visited;
+  return history_of_positions.size();
 }
-
 
 int main() {
   int input = read_file("./input.txt");
