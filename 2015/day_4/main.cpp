7cdeba1f5f7b811ecba158dcdc578c986fd2455a
alaidine <aa.alimlinde@gmail.com>
2023-03-07 22:35:08 +0400

    update about the solution of day_4

--- 2015/day_4/main.cpp
+++ 2015/day_4/main.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <md5.h>
 
 using namespace std;
 
 int main() {
-    ifstream file("./input.txt");
-    string line;
+  string input = "bgvyzdsv";
 
-    while(getline(file, line)) {
-        cout << line << endl;
+  for (int i = 0; i < 1000000; i++) {
+    string md5_hash = md5(input + itoa(i));
+
+    if (md5_hash.substr(0, 5) == "00000") {
+      std::cout << "the answer is: " << md5_hash << std::endl;
+      break;
     }
+  }
 
-    return 0;
+  return 0;
 }
