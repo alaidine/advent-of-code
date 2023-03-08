ed2087b413619f7789eb69f62373a756043313bf
alaidine <aa.alimlinde@gmail.com>
2023-03-08 17:39:52 +0400

    2015-day_4: the second part of the puzzle has been completed

--- 2015/day_4/main.cpp
+++ 2015/day_4/main.cpp
@@ -5,7 +5,7 @@
 #include "md5.h"
 
 int main() {
-  for (int i = 0; i < 1000000; i++) {
+  for (int i = 0; i < 1000000000; i++) {
     std::stringstream ss;
     std::string str;
 
@@ -14,9 +14,10 @@ int main() {
 
     std::string input = "bgvyzdsv" + str;
 
-    if (md5(input).substr(0, 5) == "00000") {
+    if (md5(input).substr(0, 6) == "000000") {
       std::cout << md5(input) << std::endl;
       std::cout << "the answer is: " << i << std::endl;
+      break;
     }
   }
 
