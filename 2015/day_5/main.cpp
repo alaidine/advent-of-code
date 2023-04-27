05746644dc2d6d6b1fd5caf6b075347fda843781
alaidine <aa.alimlinde@gmail.com>
2023-04-27 20:34:49 +0400

    updated the solution of day 5 of 2015

--- 2015/day_5/main.cpp
+++ 2015/day_5/main.cpp
@@ -1,5 +1,32 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+bool is_string_nice(string input) {
+	// check if a string is nice
+
+	return true;
+}
+
+void read_file(string input) {
+	fstream file(input);
+	int ok_strings_counter = 0;
+
+	if (file.is_open()) {
+		string line;
+		while (getline(file, line)) {
+			// the new line character is not included in the getline() function
+			cout << line << endl;
+
+			if (is_string_nice(line)) ok_strings_counter++; 
+		}
+	}
+
+	cout << endl << "number of ok strings: " << ok_strings_counter << endl;
+}
 
 int main() {
-	std::cout << "Hello World!" << std::endl;
+	read_file("input.txt");
 }
