8f91010d0384d131991ed88b053a3e7c385f1cc4
alaidine <aa.alimdlinde@gmail.com>
2024-03-31 16:55:04 +0400

    update

---
+++ 2015/day6/main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void)
+{
+    string str;
+
+    freopen("input", "r", stdin);
+
+    while (getline(cin, str)) {
+        cout << str << endl;
+    }
+}
